2d3bd9d137798d43dd8399d9bddb51b11502d335
Stark <azziahtas@gmail.com>
2017-02-22 15:09:33 +0530

    testing cors

--- app.js
+++ app.js
@@ -32,15 +32,13 @@ var LocalStrategy   = require('passport-local').Strategy;
 app.use(cookieParser());
 
 //***********************************************************
-/*
 app.use(function(req, res, next) {
     //res.header("Access-Control-Allow-Origin", "http://localhost:"+config.client);
-    res.header("Access-Control-Allow-Origin", "http://bookkaro.byethost15.com");
+    res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
     next();
-});*/
-
+});
 //Routes==============
 //App MiddleWare For All Routes
 app.all('*',function (req,res,next) {
