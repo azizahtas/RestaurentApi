92891400f9995d9cceea109c112dfd470b6a0cbf
azizahtas <azizahtas@gmail.com>
2017-06-07 15:13:32 +0530

    Added Authentication For Category

--- Controllers/CategoryController.js
+++ Controllers/CategoryController.js
@@ -15,9 +15,18 @@ CategoryRouter
             else{res.json({'success': true, 'msg' : 'We Found What your looking For!',data:Categories});}
         });
     })
-    .post('/',function (req, res) {
+    .post('/',        
+        passport.authenticate('jwt',
+        {
+            session: false
+        }),
+        function (req, res, next) {
+                var token = this.getToken(req.headers);
+                if (token) {
+                    var decoded = jwt.decode(token, config.secret);
+                    User.userExistsId(decoded, function (err, user) {
+                            if (user.otherDetails.who) {
         var Itm = req.body;
-
         Category.addCategory(Itm,function (err, Category) {
             if(err){
                 console.log('Error Saving Category :'+err);
@@ -26,7 +35,11 @@ CategoryRouter
             else{
             res.json({'success': true, 'msg' : Category.Name + ' Saved Successfully', data:[]});}
         });
-    });
+    }
+
+});
+                }
+        });
 
 //All Routes with /:id
 CategoryRouter
@@ -37,22 +50,46 @@ CategoryRouter
             res.json({'success': true, 'msg' : 'Found Category with Id : '+id,data:data});
         });
     })
-    .delete('/:_id',function (req, res) {
-        var id = req.params['_id'];
-        Category.deleteCategoryById(id,function (err,data) {
-            if(err){console.log('Error :'+err); res.json({'success': false, 'msg' : 'Error Deleting Category with Id : '+id,data:[]});}
-            else{res.json({'success': true, 'msg' : data.Name + ' Deleted Successfully',data:[]});}
-
-        });
-    })
-    .put('/:_id',function (req, res) {
-        var id = req.params['_id'];
-        var rec_proj = req.body;
-        Category.UpdateCategory(id,rec_proj,function (err,Category) {
-            if(err){console.log('Error :'+err); res.json({'success': false, 'msg' : 'Error Editing Category with Id : '+id,data:[]});}
-            res.json({'success': true, 'msg' : Category.Name+ ' Updated Successfully',data:[]});
-        });
-    });
+    .delete('/:_id',
+         passport.authenticate('jwt',
+        {
+            session: false
+        }),
+        function (req, res, next) {
+                var token = this.getToken(req.headers);
+                if (token) {
+                    var decoded = jwt.decode(token, config.secret);
+                    User.userExistsId(decoded, function (err, user) {
+                            if (user.otherDetails.who) {
+                                var id = req.params['_id'];
+                                Category.deleteCategoryById(id,function (err,data) {
+                                    if(err){console.log('Error :'+err); res.json({'success': false, 'msg' : 'Error Deleting Category with Id : '+id,data:[]});}
+                                    else{res.json({'success': true, 'msg' : data.Name + ' Deleted Successfully',data:[]});}
+                                });
+                            }
+                    });
+                }
+        })
+        .put('/:_id', passport.authenticate('jwt',
+        {
+            session: false
+        }),
+        function (req, res, next) {
+                var token = this.getToken(req.headers);
+                if (token) {
+                    var decoded = jwt.decode(token, config.secret);
+                    User.userExistsId(decoded, function (err, user) {
+                            if (user.otherDetails.who) {
+                                var id = req.params['_id'];
+                                var rec_proj = req.body;
+                                Category.UpdateCategory(id,rec_proj,function (err,Category) {
+                                    if(err){console.log('Error :'+err); res.json({'success': false, 'msg' : 'Error Editing Category with Id : '+id,data:[]});}
+                                    res.json({'success': true, 'msg' : Category.Name+ ' Updated Successfully',data:[]});
+                                });
+                            }
+                    });
+                }
+        })
 
 //Misc Routes
 CategoryRouter
